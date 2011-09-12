f167e04cbd0ab497de13be04bd5506318dfb8565
Denis Krienbühl <denis.krienbuehl@gmail.com>
2011-09-12 14:44:01 +0200

    Adds defaults to allocation and reservation

--- seantis/reservation/allocate.py
+++ seantis/reservation/allocate.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+from datetime import timedelta
+
 from five import grok
 from zope import schema
 from zope.interface import Interface
@@ -6,23 +9,28 @@ from z3c.form import field
 from z3c.form import button
 
 from seantis.reservation import _
-from seantis.reservation import raster
 from seantis.reservation import resource
+from seantis.reservation.raster import rasterize_start
+from seantis.reservation.raster import VALID_RASTER_VALUES
 
 class IAllocateForm(Interface):
 
     start = schema.Datetime(
-        title=_(u'From')
+        title=_(u'From'),
+        default=rasterize_start(datetime.today(), 60)
         )
 
     end = schema.Datetime(
-        title=_(u'To')
+        title=_(u'To'),
+        default=rasterize_start(datetime.today(), 60) + timedelta(minutes=60)
         )
 
     raster = schema.Choice(
         title=_(u'Raster'),
-        values=raster.VALID_RASTER_VALUES
+        values=VALID_RASTER_VALUES,
+        default=60
         )
+
 
 class AllocateForm(form.Form):
     grok.context(resource.IResource)
--- seantis/reservation/reserve.py
+++ seantis/reservation/reserve.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from dateutil.parser import parse as parseiso
+from datetime import timedelta
 
 from five import grok
 from zope import schema
@@ -10,15 +10,18 @@ from z3c.form import button
 
 from seantis.reservation import _
 from seantis.reservation import resource
+from seantis.reservation.raster import rasterize_start
 
 class IAllocateForm(Interface):
 
     start = schema.Datetime(
-        title=_(u'From')
+        title=_(u'From'),
+        default=rasterize_start(datetime.today(), 60)
         )
 
     end = schema.Datetime(
-        title=_(u'To')
+        title=_(u'To'),
+        default=rasterize_start(datetime.today(), 60) + timedelta(minutes=60)
         )
 
 class AllocateForm(form.Form):
@@ -34,7 +37,7 @@ class AllocateForm(form.Form):
     ignoreContext = True
 
     @button.buttonAndHandler(_(u'Reserve'))
-    def allocate(self, action):
+    def reserve(self, action):
         data, errors = self.extractData()
         if errors:
             self.status = self.formErrorsMessage
