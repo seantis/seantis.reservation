65175fb43736354a47d5140f7cbdd3e038ff086c
Denis Krienbühl <denis.krienbuehl@gmail.com>
2011-09-14 10:38:05 +0200

    Simplyfies context menu

--- seantis/reservation/js/context.js
+++ seantis/reservation/js/context.js
@@ -8,20 +8,11 @@ seantis.pad = function (val, len) {
 };
 
 seantis.eventRender = function(event, element) {
-    var title = '';
-    title += seantis.pad(event.start.getDay()) + '.';
-    title += seantis.pad(event.start.getMonth() + 1) + '.';
-    title += event.start.getFullYear() + '&nbsp;';
-    title += seantis.pad(event.start.getHours()) + ':';
-    title += seantis.pad(event.start.getMinutes()) + '&nbsp;-&nbsp;';
-    title += seantis.pad(event.end.getHours()) + ':';
-    title += seantis.pad(event.end.getMinutes());
-
     var reserve = '<a href="' + event.url + '">'
     reserve += seantis.locale('reserve') + '</a>';
 
     element.miniTip({
-        title: title,
+        title: '',
         content: reserve,
         anchor: 'e',
         event: 'hover',
