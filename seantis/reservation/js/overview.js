e698716b374459b44f9b76382a515eb5e6d1a8f2
Denis Krienbühl <denis.krienbuehl@gmail.com>
2011-11-10 11:19:05 +0100

    Adds comments to the now even more decoupled calendar day rendering in overview.js

--- seantis/reservation/js/overview.js
+++ seantis/reservation/js/overview.js
@@ -1,32 +1,49 @@
+/*
+    CalendarDays helps managing the different day cells on a fullcalendar grid.
+    It is initialized like so:
+
+    days = new CalendarDays(options);
+
+    The options paramter is an option object meant to be passed to fullcalendar
+    1.6+ which offers the dayrender callback
+*/
 var CalendarDays = function (options) {
-    var cleared = false;
+
+    // Object to store the dates and the corresponding cell element
     var store = {};
-    var get_dates =  function() { return _.keys(store); };
-    var get_cells =  function() { return _.values(store); };
-    var key = function(date) {
-        return date.getFullYear() + '-' + date.getMonth() + '-' + date.getDate();
-    };
 
+    // Indicates that the store needs clearing (see clear)
+    var cleared = false;
+
+    // Returns all the cells in the store
+    var cells =  function() { return _.values(store); };
+
+    // Key function with which a date is stored
+    var key = function(date) { return date.toDateString(); };
+
+    // Fullcalendar only creates the cells once, so in order for them to be
+    // be used differently in for each datereange they need to be cleared
+    // after the loading callback of the fullcalendar has been fired
+    // As dayRender is fired before loading, the clearing is actually
+    // done once before dayRender is called again
     var clear = function() {
-        var cells = get_cells();
-        for(var i=0; i<cells.length; i++) {
-            var cell = cells[i];
-
+        _.each(cells(), function(cell) {
             var old_classes = cell.data('old_classes');
             if (old_classes) {
                 cell.removeClass(old_classes);
                 cell.removeData('old_classes');
             }
 
-            cell.unbind('mouseenter');
-            cell.unbind('mouseleave');
-        }
-
+            cell.unbind('mouseenter.days');
+            cell.unbind('mouseleave.days');    
+        });
+        
         return true;
     };
 
+    // Add a cell to the store. Clearing is done once first, if needed
     var add = function(date, cell) {
-        if (!cleared)
+        if (!cleared) 
             cleared = clear();
             
         store[key(date)] = cell;
@@ -36,13 +53,34 @@ var CalendarDays = function (options) {
         return store[key(date)]; 
     };
 
+    //Make these functions public
     this.add = add;
     this.get = get;
 
-    options.loading = function() { 
-        cleared=false; 
+    //Adds a class to a date cell and stores the result for later cleaning
+    this.addClass = function(date, classes) {
+        var cell = get(date);
+        cell.addClass(classes);
+        cell.data('old_classes', classes); 
     };
-    options.dayRender = function(date, element) { add(date, element); };
+
+    this.mouseenter = function(date, callback) {
+        var cell = get(date);
+        cell.bind('mouseenter.days', callback);
+    };
+
+    this.mouseleave = function(date, callback) {
+        var cell = get(date);
+        cell.bind('mouseleave.days', callback);
+    };
+
+    options.loading = function() { 
+        cleared=false; //indicate that the next add call has to clear the store
+    };
+
+    options.dayRender = function(date, element) { 
+        add(date, element); 
+    };
 };
 
 (function($) {
@@ -52,9 +90,6 @@ var CalendarDays = function (options) {
             return;
 
         var overview = $(seantis.overview.id);
-
-        var options = {};
-        var days = new CalendarDays(options);
 
         var get_items = function(uuids) {
             var uuidmap = options.uuidmap;
@@ -68,23 +103,21 @@ var CalendarDays = function (options) {
 
         var render = function(event, element) {
             var cell = days.get(event.start);
-            if (!_.isUndefined(cell) && ! cell.data('old_classes')) {
+            if (!_.isUndefined(cell)) {
                 var items = get_items(event.uuids);
                 var itemids = '#' + items.join(', #');
                 var classes = event.className.join(' ') + ' ' + items.join(' ');
 
-                cell.addClass(classes);
-                cell.data('old_classes', classes); 
-                
-                cell.bind('mouseenter', function() {
+                days.addClass(event.start, classes);
+                days.mouseenter(event.start, function() {
                     highlight_group($(itemids), true);
                     highlight_group($(cell), true);
                 });
                 
-                cell.bind('mouseleave', function() {
+                days.mouseleave(event.start, function() {
                     highlight_group($(itemids), false);
                     highlight_group($(cell), false);
-                });
+                });   
             }
 
             return false;
@@ -102,19 +135,22 @@ var CalendarDays = function (options) {
         var item_mouseout = function() {
             var element = $('.' + $(this).attr('id'));
             highlight_group(element, false);
-        };  
-
-        var dayrender = function(date, element, view) {
-            days.add(date, element);
         };
 
-        var isloading = function() {
-            days.cleared = false;
-        };
+        var directoryResult = $('.directoryResult');
+        directoryResult.mouseenter(function() {
+            var element = $('.' + $(this).attr('id'));
+            highlight_group(element, true);
+        });
 
-        $('.directoryResult').mouseenter(item_mouseover);
-        $('.directoryResult').mouseleave(item_mouseout);
+        directoryResult.mouseleave(function() {
+            var element = $('.' + $(this).attr('id'));
+            highlight_group(element, false);
+        });
         
+        var options = {};
+        var days = new CalendarDays(options);
+
         $.extend(options, {
             firstDay: 1,
             timeFormat: 'HH:mm{ - HH:mm}',
