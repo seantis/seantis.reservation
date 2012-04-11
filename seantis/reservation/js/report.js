0c633b3a8c7bb53d23916bfc9f5d3b517af88037
Denis Krienbühl <denis.krienbuehl@gmail.com>
2012-04-11 16:12:28 +0200

    Adds ability to show/hide resources in the report view

--- seantis/reservation/js/report.js
+++ seantis/reservation/js/report.js
@@ -8,6 +8,31 @@
         var show_details = function(elements, show) {
             elements.toggleClass('show-details', show);  
         };
+        
+        var show_resource = function(uuid, show) {
+            var resources = report.find('.resource[data-uuid="' + uuid + '"]');
+            resources.toggle(show);
+
+            _.each(resources, function(resource) {
+                var day = $(resource).parent();
+                var show_day;
+
+                if (day.is(':visible')) {
+                    // if the day is visible we have a shortcut to figure out
+                    // if any resources below the day are visible..
+                    show_day = day.find('.resource:visible').length > 0;
+                } else {
+                    // ..which doesn't work if the day itself is invisible,
+                    // as the children will be considered invisible in any case
+                    show_day = _.any(day.find('.resource'), function(child) {
+                        return $(child).css('display') != 'none';
+                    });
+                }
+                    
+
+                day.toggle(show_day);
+            });
+        };
 
         // Toggle reservation details
         report.find('.reservation').click(function() {
@@ -18,6 +43,14 @@
         $('.controlbox input[name="details"]').change(function() {
             var show = $(this).val() == 'show';
             show_details(report.find('.reservation'), show);
+        });
+        
+        // Toggle specific resources
+        $('.controlbox .resource-checkbox').change(function() {
+            var uuid = $(this).val();
+            var show = $(this).is(':checked');
+            
+            show_resource(uuid, show);
         });
 
         // Hookup approve/decline overlays
