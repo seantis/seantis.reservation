84a2dbdaaa760b44f68eeb1af51674fcc362c488
Denis Krienbühl <denis.krienbuehl@gmail.com>
2012-04-24 13:47:17 +0200

    Adds ability to remove orphan records in the control panel

--- seantis/reservation/settings.py
+++ seantis/reservation/settings.py
@@ -1,3 +1,8 @@
+import logging
+logger = logging.getLogger('seantis.reservation')
+
+from sqlalchemy import not_
+
 from zope import schema
 from zope.interface import Interface
 from zope.component import getUtility
@@ -7,6 +12,18 @@ from plone.registry.interfaces import IRegistry
 from plone.app.registry.browser.controlpanel import RegistryEditForm
 from plone.app.registry.browser.controlpanel import ControlPanelFormWrapper
 
+from plone.uuid.interfaces import IUUID
+
+from Products.Five.browser.pagetemplatefile import ZopeTwoPageTemplateFile
+from Products.CMFCore.utils import getToolByName
+
+from z3c.form.group import Group
+from z3c.form.field import Fields
+
+from seantis.reservation.models import Allocation, ReservedSlot, Reservation
+from seantis.reservation import Session
+from seantis.reservation import utils
+from seantis.reservation.interfaces import IResource
 from seantis.reservation import _
 
 class ISeantisReservationSettings(Interface):
@@ -36,9 +53,83 @@ def get(name, default=None):
     assert hasattr(settings, name), "Unknown setting: %s" % name
     return getattr(settings, name)
 
+class SettingsGroup(Group):
+    label = _(u'Settings')
+    fields = Fields(ISeantisReservationSettings)
+
 class SeantisReservationSettingsPanelForm(RegistryEditForm): 
     schema = ISeantisReservationSettings
     label = _(u"Seantis Reservation Control Panel")
+    groups = (SettingsGroup, )
+
+    template = ZopeTwoPageTemplateFile('templates/controlpanel.pt')
+
+    enable_form_tabbing = False
+
+    def number_of_records(self):
+        return sum((
+            Session.query(Allocation).count(),
+            Session.query(ReservedSlot).count(),
+            Session.query(Reservation).count()
+        ))
+
+    def existing_uuids(self):
+        # get a list of all resource uuids
+        catalog = getToolByName(self.context, 'portal_catalog')
+        brains = catalog.unrestrictedSearchResults(
+            object_provides=IResource.__identifier__
+        )
+
+        return map(lambda brain: IUUID(brain.getObject()), brains)
+
+    def number_of_orphan_records(self):
+        uuids = self.existing_uuids()
+
+        return sum((
+            Session.query(Allocation).filter(not_(Allocation.mirror_of.in_(uuids))).count(),
+            Session.query(ReservedSlot).filter(not_(ReservedSlot.resource.in_(uuids))).count(),
+            Session.query(Reservation).filter(not_(Reservation.resource.in_(uuids))).count()
+        ))
+
+    def remove_orphan_records(self):
+        uuids = self.existing_uuids()
+        count = self.number_of_orphan_records()
+
+        logger.info('Removing %i Orphan Records', count)
+
+        allocations = Session.query(Allocation).filter(not_(Allocation.mirror_of.in_(uuids)))
+        slots = Session.query(ReservedSlot).filter(not_(ReservedSlot.resource.in_(uuids)))
+        reservations = Session.query(Reservation).filter(not_(Reservation.resource.in_(uuids)))
+
+        # be very paranoid about this by double-checking the uuids
+        dead_uuids = set()
+        for reservation in reservations:
+            dead_uuids.add(reservation.resource)
+        for slot in slots:
+            dead_uuids.add(slots.resource)
+        for allocation in allocations:
+            dead_uuids.add(allocation.mirror_of)
+
+        for dead_uuid in dead_uuids:
+            if utils.get_resource_by_uuid(utils.getSite(), dead_uuid) != None:
+                raise AssertionError('Tried to Delete a Non-Orphan Record (uuid: %s)' % dead_uuid)
+
+        reservations.delete('fetch')
+        slots.delete('fetch')
+        allocations.delete('fetch')
+
+        return count
+
+    def update(self, *args, **kwargs):
+        super(SeantisReservationSettingsPanelForm, self).update(*args, **kwargs)
+        if self.request.get('form.actions.remove_orphans'):
+            count = self.remove_orphan_records()
+            utils.flash(self.context, 
+                _(u'${count} Orphan Records Removed', mapping={
+                    'count': count
+                })
+            )
+
     
 SeantisReservationControlPanelView = layout.wrap_form(
         SeantisReservationSettingsPanelForm, ControlPanelFormWrapper
