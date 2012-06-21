391fb449ec579a4965f047d700fd0b45286d468f
Denis Krienbühl <denis.krienbuehl@gmail.com>
2012-06-21 15:25:28 +0200

    Improves upgrade documentation and code

--- seantis/reservation/upgrades.py
+++ seantis/reservation/upgrades.py
@@ -1,37 +1,41 @@
-from seantis.reservation import ORMBase
-from seantis.reservation import Session
-from seantis.reservation.session import serialized
+from sqlalchemy import create_engine
 
 from alembic.migration import MigrationContext
 from alembic.operations import Operations
 from alembic.autogenerate import compare_metadata
 
-from StringIO import StringIO
-
-from sqlalchemy import create_engine
-from sqlalchemy import Column
-from sqlalchemy import types
-
+from seantis.reservation import ORMBase
 from seantis.reservation import utils
-import transaction
 
 def upgrade_1_to_1000(setuptools):
+    """ Upgrades seantis.reservation, adding modified and created dates to
+    each table in the database.
 
-    engine = create_engine(utils.get_config('dsn'),
-        isolation_level='SERIALIZABLE'
-    )
+    The upgrade uses alembic to create a diff between the existing database
+    and the metadata.
+
+    Alembic really is a tool for much more but for now this should cover it.
+    Plus I'd like to be able to run the database upgrades through the
+    add-on interface as administrator.
+
+    """
+
+    engine = create_engine(utils.get_config('dsn'), isolation_level='SERIALIZABLE')
     connection = engine.connect()
     trans = connection.begin()
 
     try:
-        context = MigrationContext.configure(connection, opts=dict(output_buffer=StringIO()))
+
+        context = MigrationContext.configure(connection)
         diff = compare_metadata(context, ORMBase.metadata)
         op = Operations(context)
 
+        # go through diff and execute the changes on the operations object
         for method, table, col in diff:
             getattr(op, method)(table, col.copy())
     
         trans.commit()
+
     except:
         trans.rollback()
         raise
