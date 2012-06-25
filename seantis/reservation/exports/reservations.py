dc42449282e2ba209925bb878c1fdbde21b4fd51
Denis Krienbühl <denis.krienbuehl@gmail.com>
2012-06-25 11:36:35 +0200

    Adds comments to reservation export

--- seantis/reservation/exports/reservations.py
+++ seantis/reservation/exports/reservations.py
@@ -8,17 +8,27 @@ from seantis.reservation import Session
 from seantis.reservation import utils
 from seantis.reservation.models import Reservation
 
-
 def translator(language):
+    """ Returns a function which will return the translation for a given text
+    in the once defined language.
+
+    """
     def curried(text):
         return i18n.translate(text, target_language=language)
 
     return curried
 
 def dataset(context, request, resources, language):
+    """ Takes a list of resources and returns a tablib dataset filled with
+    all reservations of these resources. The json data of the reservations
+    is filled using a single column for each type (form + field).
+
+    """
+
     text = translator(language)
     reservations = fetch_records(resources)
 
+    # create the headers
     headers = [
         text(_(u'Parent')),
         text(_(u'Resource')),
@@ -31,11 +41,16 @@ def dataset(context, request, resources, language):
     baseheaders = len(headers)
     headers.extend(additional_headers(reservations))
 
+    # for each reservation get a record per timeslot (which is a single slot
+    # for reservations targeting an allocation and n slots for a reservation
+    # targeting a group)
     records = []
     for r in reservations:
+
         token = utils.string_uuid(r.token)
         resource = resources[utils.string_uuid(r.resource)]
         parent = resource.parent()
+        
         for start, end in r.timespans():
             record = [
                 resource.title,
@@ -50,6 +65,7 @@ def dataset(context, request, resources, language):
 
             records.append(record)
 
+    # put the results in a tablib dataset
     ds = tablib.Dataset()
     ds.headers = headers
     for r in records:
@@ -58,6 +74,7 @@ def dataset(context, request, resources, language):
     return ds
 
 def fetch_records(resources):
+    """ Returns the records used for the dataset. """
     query = Session.query(Reservation)
     query = query.filter(Reservation.resource.in_(resources.keys()))
     query = query.order_by(
@@ -71,22 +88,34 @@ def fetch_records(resources):
     return query.all()
 
 def fieldkey(form, field):
+    """ Returns the fieldkey for any given json data field + form. """
     return '%s.%s' % (form["desc"], field["desc"])
 
 def additional_headers(reservations):
+    """ Go through all reservations and build a list of all possible headers. """
     formdata = [r.data.values() for r in reservations]
-
+    
     headers = []
     for forms in formdata:
         for form in forms:
             for field in sorted(form["values"], key=lambda f: f["sortkey"]):
+
+                # A set could be used here, but then a separate step for
+                # sorting would be needed
                 key = fieldkey(form, field)
-                if not key in headers:
+                if not key in headers: 
                     headers.append(key)
 
     return headers
 
 def additional_columns(reservation, headers):
+    """ Given a reservation and the list of additional headers return a list
+    of columns filled with either None or the value of the json data.
+
+    The resulting list will always be of the same length as the given headers
+    list. 
+
+    """
     forms = reservation.data.values()
 
     columns = [None] * len(headers)
