db3826035cf8e8c7e2b0f302523ef0a0a27c584b
Denis Krienbühl <denis.krienbuehl@gmail.com>
2012-09-04 11:57:38 +0200

    Adds database config example to the buildout bash script

--- buildout/buildouts.sh
+++ buildout/buildouts.sh
@@ -4,10 +4,17 @@ if which wget &> /dev/null; then
     wget https://raw.github.com/seantis/seantis.reservation/master/buildout/buildout.cfg -O buildout.cfg
     wget https://raw.github.com/seantis/seantis.reservation/master/buildout/versions.cfg -O versions.cfg
     wget https://raw.github.com/seantis/seantis.reservation/master/buildout/develop.cfg -O develop.cfg
+    wget https://raw.github.com/seantis/seantis.reservation/master/buildout/database.cfg.example -O database.cfg.example
+
 fi
 
 if which curl &> /dev/null; then
     curl https://raw.github.com/seantis/seantis.reservation/master/buildout/buildout.cfg -o buildout.cfg
     curl https://raw.github.com/seantis/seantis.reservation/master/buildout/versions.cfg -o versions.cfg
     curl https://raw.github.com/seantis/seantis.reservation/master/buildout/develop.cfg -o develop.cfg  
+    curl https://raw.github.com/seantis/seantis.reservation/master/buildout/database.cfg.example -o database.cfg.example
+fi
+
+if [ ! -f database.cfg ]; then
+    cp database.cfg.example database.cfg
 fi
