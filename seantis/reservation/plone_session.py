ca8f7049ea7ef35b23b4b1caba64dba1d121a38c
Tobias Reinhard <tobias.reinhard@seantis.ch>
2012-12-10 11:16:03 +0100

    Encapsulate session key generation in a function.

--- seantis/reservation/plone_session.py
+++ seantis/reservation/plone_session.py
@@ -24,21 +24,23 @@ def set_session(context, key, value):
     session[key] = value
 
 
-def get_email(context):
+def session_key(name):
     plone = getSite()
-    sid = '%s:reservation:email' % plone.id
+    return '%s:reservation:%s' % (plone.id, name)
+
+
+def get_email(context):
+    sid = session_key('email')
     return get_session(context, sid) or None
 
 
 def get_additional_data(context):
-    plone = getSite()
-    sid = '%s:reservation:additional_data' % plone.id
+    sid = session_key('additional_data')
     return get_session(context, sid) or None
 
 
 def get_session_id(context):
-    plone = getSite()
-    sid = '%s:reservation:session_id' % plone.id
+    sid = session_key('session_id')
     session_id = get_session(context, sid)
     if session_id is None:
         session_id = uuid.uuid4()
@@ -47,12 +49,10 @@ def get_session_id(context):
 
 
 def set_email(context, email):
-    plone = getSite()
-    sid = '%s:reservation:email' % plone.id
+    sid = session_key('email')
     set_session(context, sid, email)
 
 
 def set_additional_data(context, data):
-    plone = getSite()
-    sid = '%s:reservation:additional_data' % plone.id
+    sid = session_key('additional_data')
     set_session(context, sid, data)
