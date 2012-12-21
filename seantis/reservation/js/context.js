8ee94426b27815ee563947934d8ee2dd6a82f035
Tobias Reinhard <tobias.reinhard@seantis.ch>
2012-12-21 13:15:11 +0100

    JSLint run.

--- seantis/reservation/js/context.js
+++ seantis/reservation/js/context.js
@@ -3,9 +3,10 @@ if (!this.seantis) this.seantis = {};
 // adds a context menu to an event
 seantis.contextmenu = function(event, element, calendar) {
     var content = seantis.contextmenu.build(event);
-    if (!content) 
+    if (!content) {
         return;
-        
+    }
+
     // if the menu only has one possible link it is displayed like a tooltip
     // indicating what action will be executed when the event is clicked
     // (see render_event in calendar.js)
@@ -13,14 +14,14 @@ seantis.contextmenu = function(event, element, calendar) {
     var delay = trigger === 'hover' ? 200 : 0;
     var aHide = trigger === 'hover' ? false : true;
 
-    // add either overlay or inline loading for the links 
+    // add either overlay or inline loading for the links
     // depending on the generated content html
     var render_menu = function(element) {
         $.each($('a', element), function(ix, link) {
                 var $link = $(link);
 
                 switch ($link.attr('data-target')) {
-                    
+
                     case "overlay":
                         calendar.overlay_init($link);
                         break;
@@ -48,25 +49,27 @@ seantis.contextmenu = function(event, element, calendar) {
         fadeIn: 100,
         fadeOut: 100,
         render: render_menu
-    });  
+    });
 };
 
 // returns false if the event has more than one menu item
 // returns the url of the menu item if there's only one
 seantis.contextmenu.simple = function(event) {
 
-    if (event.menuorder.length > 1) 
+    if (event.menuorder.length > 1) {
         return false;
+    }
 
-    if (event.menu[event.menuorder[0]].length > 1)
+    if (event.menu[event.menuorder[0]].length > 1) {
         return false;
+    }
 
-    return event.menu[event.menuorder[0]][0].url;    
+    return event.menu[event.menuorder[0]][0].url;
 };
 
 // closes the menu
 seantis.contextmenu.close = function() {
-    $.fn.miniTip({doHide:true});  
+    $.fn.miniTip({doHide:true});
 };
 
 // builds the menu html from any object with the following interface
@@ -90,15 +93,16 @@ seantis.contextmenu.build = function(event) {
                                <%= name %></a>');
 
     var simple = seantis.contextmenu.simple(event) !== false;
-    
+
     var html = '';
     for (var i=0; i<event.menuorder.length; i++) {
 
         var group = event.menuorder[i];
-            
+
         // if there's only a single link the group is not displayed
-        if (!simple && group.length > 0)
+        if (!simple && group.length > 0) {
             html += title({text:group});
+        }
 
         for (var j=0; j<event.menu[group].length; j++) {
             var item = event.menu[group][j];
