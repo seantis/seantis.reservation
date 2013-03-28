ac8d7c5c40be85d03ee37babc43e6e067a4db2ac
Denis Krienbühl <denis.krienbuehl@gmail.com>
2013-03-28 13:49:01 +0100

    Adds upgrade step for new profiles

--- seantis/reservation/upgrades.py
+++ seantis/reservation/upgrades.py
@@ -1,3 +1,6 @@
+import logging
+log = logging.getLogger('seantis.reservation')
+
 from functools import wraps
 
 from alembic.migration import MigrationContext
@@ -9,6 +12,7 @@ from sqlalchemy import MetaData
 from sqlalchemy import Table
 from sqlalchemy.schema import Column
 
+from Products.CMFCore.utils import getToolByName
 from zope.component import getUtility
 
 from seantis.reservation import utils
@@ -54,8 +58,8 @@ def upgrade_to_1001(operations, metadata):
 
     reservations_table = Table('reservations', metadata, autoload=True)
     if 'session_id' not in reservations_table.columns:
-        operations.add_column('reservations',
-            Column('session_id', customtypes.GUID())
+        operations.add_column(
+            'reservations', Column('session_id', customtypes.GUID())
         )
 
 
@@ -64,9 +68,9 @@ def upgrade_1001_to_1002(operations, metadata):
 
     reservations_table = Table('reservations', metadata, autoload=True)
     if 'quota' not in reservations_table.columns:
-        operations.add_column('reservations',
-            Column('quota',
-                types.Integer(), nullable=False, server_default='1'
+        operations.add_column(
+            'reservations', Column(
+                'quota', types.Integer(), nullable=False, server_default='1'
             )
         )
 
@@ -76,8 +80,49 @@ def upgrade_1002_to_1003(operations, metadata):
 
     allocations_table = Table('allocations', metadata, autoload=True)
     if 'reservation_quota_limit' not in allocations_table.columns:
-        operations.add_column('allocations',
-            Column('reservation_quota_limit',
+        operations.add_column(
+            'allocations', Column(
+                'reservation_quota_limit',
                 types.Integer(), nullable=False, server_default='0'
             )
         )
+
+
+def upgrade_1003_to_1004(context):
+
+    # 1004 untangles the dependency hell that was default <- sunburst <- izug.
+    # Now, sunburst and izug.basetheme both have their own profiles.
+
+    # Since the default profile therefore has only the bare essential styles
+    # it needs to be decided on upgrade which theme was used, the old css
+    # files need to be removed and the theme profile needs to be applied.
+
+    # acquire the current theme
+    skins = getToolByName(context, 'portal_skins')
+    theme = skins.getDefaultSkin()
+
+    # find the right profile to use
+    profilemap = {
+        'iZug Base Theme': 'izug_basetheme',
+        'Sunburst Theme': 'sunburst'
+    }
+
+    if theme not in profilemap:
+        log.info("Theme %s is not supported by seantis.reservation" % theme)
+        profile = 'default'
+    else:
+        profile = profilemap[theme]
+
+    # remove all existing reservation stylesheets
+    css_registry = getToolByName(context, 'portal_css')
+    stylesheets = css_registry.getResourcesDict()
+    ids = [i for i in stylesheets if 'resource++seantis.reservation.css' in i]
+
+    map(css_registry.unregisterResource, ids)
+
+    # reapply the chosen profile
+
+    setup = getToolByName(context, 'portal_setup')
+    setup.runAllImportStepsFromProfile(
+        'profile-seantis.reservation:%s' % profile
+    )
