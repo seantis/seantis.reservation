5db15ea882053f5fcdb2eb9f06ab1429721a0622
David Erni <david.erni@4teamwork.ch>
2013-06-04 09:29:57 +0200

    Use correct uuid in settings panel.
    Conflicts:
    	seantis/reservation/settings.py

--- seantis/reservation/settings.py
+++ seantis/reservation/settings.py
@@ -86,6 +86,95 @@ class SeantisReservationSettingsPanelForm(RegistryEditForm):
 
     template = ZopeTwoPageTemplateFile('templates/controlpanel.pt')
 
+    def number_of_records(self):
+        return sum((
+            Session.query(Allocation).count(),
+            Session.query(ReservedSlot).count(),
+            Session.query(Reservation).count()
+        ))
+
+    def existing_uuids(self):
+
+        def by_site(site):
+            # get a list of all resource uuids
+            catalog = getToolByName(site, 'portal_catalog')
+
+            brains = catalog.unrestrictedSearchResults(
+                object_provides=IResource.__identifier__
+            )
+            return map(lambda brain: brain.getObject().uuid(), brains)
+
+        # do this on all items in the zope instance, not just the site!
+        uuids = []
+        for site in utils.plone_sites():
+            uuids.extend(by_site(site))
+
+        return uuids
+
+    def number_of_orphan_records(self):
+        uuids = self.existing_uuids()
+
+        return sum((
+            Session.query(Allocation).filter(
+                not_(Allocation.mirror_of.in_(uuids))
+            ).count(),
+            Session.query(ReservedSlot).filter(
+                not_(ReservedSlot.resource.in_(uuids))
+            ).count(),
+            Session.query(Reservation).filter(
+                not_(Reservation.resource.in_(uuids))
+            ).count()
+        ))
+
+    def remove_orphan_records(self):
+        uuids = self.existing_uuids()
+        count = self.number_of_orphan_records()
+
+        logger.info('Removing %i Orphan Records', count)
+
+        allocations = Session.query(Allocation).filter(
+            not_(Allocation.mirror_of.in_(uuids))
+        )
+        slots = Session.query(ReservedSlot).filter(
+            not_(ReservedSlot.resource.in_(uuids))
+        )
+        reservations = Session.query(Reservation).filter(
+            not_(Reservation.resource.in_(uuids))
+        )
+
+        # be very paranoid about this by double-checking the uuids
+        dead_uuids = set()
+        for reservation in reservations:
+            dead_uuids.add(reservation.resource)
+        for slot in slots:
+            dead_uuids.add(slot.resource)
+        for allocation in allocations:
+            dead_uuids.add(allocation.mirror_of)
+
+        for dead_uuid in dead_uuids:
+            if utils.get_resource_by_uuid(dead_uuid) \
+                    is not None:
+                raise AssertionError(
+                    'Tried to Delete a Non-Orphan Record (uuid: %s)' %
+                    dead_uuid
+                )
+
+        reservations.delete('fetch')
+        slots.delete('fetch')
+        allocations.delete('fetch')
+
+        return count
+
+    def update(self, *args, **kwargs):
+        super(SeantisReservationSettingsPanelForm, self).update(
+            *args, **kwargs
+        )
+        if self.request.get('form.actions.remove_orphans'):
+            count = self.remove_orphan_records()
+            utils.flash(
+                self.context,
+                _(u'${count} Orphan Records Removed', mapping={'count': count})
+            )
 
 SeantisReservationControlPanelView = layout.wrap_form(
     SeantisReservationSettingsPanelForm, ControlPanelFormWrapper
