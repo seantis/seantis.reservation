e1af08f314d51c325fc38b7de7477c8964b88fd8
Denis Krienbühl <denis.krienbuehl@gmail.com>
2013-09-13 11:08:40 +0200

    Release 1.0.9

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 name = "seantis.reservation"
 description = "Plone addon to reserve stuff in a calendar."
-version = '1.0.8'
+version = '1.0.9'
 
 zug_require = [
     'ftw.contentmenu',
