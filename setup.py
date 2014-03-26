04064e230042c339ee023d5c17380c47347f70f5
Denis Krienbühl <denis@href.ch>
2014-03-26 11:31:18 +0100

    Prepare next release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 name = "seantis.reservation"
 description = "Plone addon to reserve stuff in a calendar."
-version = '1.0.21'
+version = '1.0.22'
 
 zug_require = [
     'ftw.contentmenu',
