6f41e1f1ca9d557d8c155d2cc946c6e000c19290
Denis Krienbühl <denis@href.ch>
2014-08-05 11:36:44 +0200

    Adds tests for combined reservations

---
+++ seantis/reservation/tests/test_reservations.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+
+from datetime import date
+
+from seantis.reservation.tests import IntegrationTestCase
+from seantis.reservation.reservations import combine_reservations
+
+
+class TestReservations(IntegrationTestCase):
+
+    def test_combined_reservations(self):
+
+        class Reservation(object):
+
+            def __init__(self, token, timespans, data):
+                self.data = data
+                self.token = token
+                self._timespans = timespans
+
+            def timespans(self):
+                return self._timespans
+
+        reservations = [
+            Reservation('a', [
+                (date(2014, 1, 1), date(2014, 1, 1)),
+                (date(2014, 1, 2), date(2014, 1, 2)),
+                (date(2014, 1, 3), date(2014, 1, 3)),
+            ], data={'id': 1}),
+            Reservation('b', [
+                (date(2015, 1, 1), date(2015, 1, 1)),
+                (date(2015, 1, 2), date(2015, 1, 2)),
+            ], data={'id': 2}),
+
+            Reservation('b', [
+                (date(2015, 1, 3), date(2015, 1, 3)),
+            ], data={'id': 3}),
+        ]
+
+        combined = list(combine_reservations(reservations))
+        self.assertEqual(len(combined), 2)
+
+        a, b = combined
+        self.assertEqual(a.reservation.data['id'], 1)
+        self.assertEqual(b.reservation.data['id'], 2)
+
+        self.assertEqual(a.timespans(), [
+            (date(2014, 1, 1), date(2014, 1, 1)),
+            (date(2014, 1, 2), date(2014, 1, 2)),
+            (date(2014, 1, 3), date(2014, 1, 3)),
+        ])
+
+        self.assertEqual(b.timespans(), [
+            (date(2015, 1, 1), date(2015, 1, 1)),
+            (date(2015, 1, 2), date(2015, 1, 2)),
+            (date(2015, 1, 3), date(2015, 1, 3)),
+        ])
