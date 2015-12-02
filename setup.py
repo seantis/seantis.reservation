58422a53fd968c696579c10cef85792b3bb7aae1
Marc Sommerhalder <marc.sommerhalder@seantis.ch>
2015-12-02 12:29:04 +0100

    Back to development

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 name = "seantis.reservation"
 description = "Plone addon to reserve stuff in a calendar."
-version = '1.4.0'
+version = '1.4.1'
 
 zug_require = [
     'ftw.contentmenu',
